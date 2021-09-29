bc002b5ecb673e6e8a29b86be7498b11d13cd1bb
sas_sas_da <fish38111183@gmail.com>
2021-09-29 15:40:09 +0900

    fix image height

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -9,7 +9,7 @@
     </header>
       
     <v-container class="d-flex flex-column align-start blog-content">
-      <v-img :height="`${imageHeight}px`" class="mb-7 mx-auto" :src="bannerPath(article)" />
+      <v-img width="100%" :minHeight="`200px`" class="mb-7 mx-auto" :src="bannerPath(article)" />
       <div style="width: 100%;" class="d-flex flex-row justify-space-between align-center mb-3">
         <time class="text-subtitle-1" v-html="formatDate(article.updatedAt)"></time>
         <div>
@@ -65,8 +65,6 @@ import moment from 'moment';
   }
 })
 export default class Page extends Vue {
-
-  readonly imageHeight = 450;
 
   article: IContentDocument | null = null;
   siteName: string = process.env.siteName || '';
